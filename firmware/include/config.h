9ad726016e1124fe48e7b0dd685b283e888b55ed
Ali Akbar Mohsennejad <pooyamohsennjad@gmail.com>
2025-08-26 11:32:44 +0330

    Create config.h

---
+++ firmware/include/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Pin definitions
+#define TRIG_PIN 9
+#define ECHO_PIN 10
+
+// DHT22 pin (only used in compensated scenario)
+#define DHT_PIN 7
+
+// Speed of sound at ~20°C in cm/µs
+#define SPEED_OF_SOUND 0.0343  
+
+#endif
